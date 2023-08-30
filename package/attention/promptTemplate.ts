88119a7ef6e4302bfabc2f5c876dea137bbfeece
少轻狂 <1677568218@qq.com>
2023-08-31 01:53:24 +0800

    formatPromptTemplate 支持插槽

--- package/attention/promptTemplate.ts
+++ package/attention/promptTemplate.ts
@@ -1,22 +1,22 @@
-import { PolishPromptTemplate } from "./polishPromptTemplate.js";
-import { AgentPromptTemplate } from "./agentPromptTemplate.js";
-import { createMessage } from "./basePromptTemplate.js";
 import {
   milvusVectorDB,
   milvusVectorDBPromptTemplateSchema,
-  messageType,
+  createMessageSchema,
+  PolishPromptTemplate,
+  AgentPromptTemplate,
+  createMessage,
 } from "@idealeap/gwt";
 export interface PromptsSchema {
   name: "polishPromptTemplate" | "agentPromptTemplate" | string;
   prompt?: MultiPromptSchema[] | any[];
-  schema?: object;
+  schema?: Record<string, any>;
   COLLECTION_NAME?: string;
 }
 export interface MultiPromptSchema
-  extends messageType,
+  extends createMessageSchema,
     milvusVectorDBPromptTemplateSchema {
   COLLECTION_NAME?: string;
-  content: string | null;
+  content: string;
 }
 
 export const formatPromptTemplate = async (prompts: PromptsSchema) => {
@@ -55,16 +55,13 @@ export const formatPromptTemplate = async (prompts: PromptsSchema) => {
               : undefined;
             return res;
           }
-          //!该方法已弃用
-          // 如果含有role字段，说明是template，获取除了role,content,name,function_call之外的字段，并对这些字段进行遍历处理，对content中{{}}进行替换。若含有vector字段，说明是milvus的promptTemplate，调用milvus的promptTemplate生成函数
-          // if ("role" in prompt && "content" in prompt && !!prompt.content) {
-          //   const { role, content, name, function_call, ...rest } = prompt;
-          //   const newContent = content.replace(
-          //     /{{(.*?)}}/g,
-          //     (_, match) => (rest as any)[match.trim()] || "",
-          //   );
-          //   return createMessage(role, newContent, name, function_call);
-          // }
+          if (
+            typeof prompt === "object" &&
+            !!prompt.contentSlots &&
+            !!prompt.content
+          ) {
+            return createMessage(prompt);
+          }
           return prompt;
         })();
       }),
