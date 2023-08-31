7e7be58ae7407391414406afb59a5bce1f441d42
少轻狂 <1677568218@qq.com>
2023-08-31 18:48:20 +0800

    fet: init pipe func

---
+++ package/collaboration/pipe/pipe.ts
@@ -0,0 +1,138 @@
+interface PipeOptions<T, R> {
+  id: string;
+  description: string;
+  dependencies?: string[];
+  onData?: (input: T, context: ChainPipeContext) => T;
+  onResult?: (result: R) => R;
+  onError?: (error: any) => void;
+  onStepComplete?: (
+    result: R,
+    step: number,
+    totalSteps: number,
+    allResults: Map<string, any>,
+  ) => void;
+}
+
+interface ChainPipeContext {
+  stepResults: Map<string, any>;
+}
+
+interface ChainPipeOptions {
+  onProgress?: (completed: number, total: number) => void;
+}
+
+class Pipe<T, R> {
+  constructor(
+    private callback: (input: T, context: ChainPipeContext) => Promise<R> | R,
+    public options: PipeOptions<T, R>,
+  ) {}
+
+  async execute(
+    input: T,
+    context: ChainPipeContext,
+    step: number,
+    totalSteps: number,
+  ): Promise<R> {
+    try {
+      // Process dependencies
+      if (this.options.dependencies) {
+        for (const dep of this.options.dependencies) {
+          if (!context.stepResults.has(dep)) {
+            throw new Error(`Dependency ${dep} not found`);
+          }
+        }
+      }
+
+      const processedInput = this.options.onData
+        ? this.options.onData(input, context)
+        : input;
+      const result = await Promise.resolve(
+        this.callback(processedInput, context),
+      );
+      const finalResult = this.options.onResult
+        ? this.options.onResult(result)
+        : result;
+
+      context.stepResults.set(this.options.id, finalResult);
+      this.options.onStepComplete?.(
+        finalResult,
+        step,
+        totalSteps,
+        context.stepResults,
+      );
+
+      return finalResult;
+    } catch (error) {
+      this.options.onError?.(error);
+      throw new Error(`Pipe ${this.options.id} failed: ${String(error)}`);
+    }
+  }
+
+  // Configuration import/export
+  static fromConfig<T, R>(
+    config: PipeOptions<T, R>,
+    callback: (input: T, context: ChainPipeContext) => Promise<R> | R,
+  ): Pipe<T, R> {
+    return new Pipe(callback, config);
+  }
+
+  toConfig(): PipeOptions<T, R> {
+    return this.options;
+  }
+}
+
+async function chainPipes(
+  pipes: Pipe<any, any>[],
+  input: any,
+  options?: ChainPipeOptions,
+): Promise<any> {
+  const context: ChainPipeContext = { stepResults: new Map() };
+  const total = pipes.length;
+
+  for (let i = 0; i < total; i++) {
+    const pipe = pipes[i];
+    try {
+      input = await pipe.execute(input, context, i + 1, total);
+      // Trigger the onProgress here
+      options?.onProgress?.(i + 1, total);
+    } catch (error) {
+      throw new Error(
+        `Chaining failed at Pipe ${pipe.options.id}: ${String(error)}`,
+      );
+    }
+  }
+
+  return context.stepResults;
+}
+
+// Example usage
+const pipe1 = Pipe.fromConfig(
+  { id: "double", description: "Doubles the input" },
+  (input: number) => input * 2,
+);
+
+const pipe2 = Pipe.fromConfig(
+  {
+    id: "toString",
+    description: "Converts to string",
+    dependencies: ["double"],
+  },
+  (input: number, context) =>
+    `Number: ${context.stepResults.get("double") || input}`,
+);
+
+const pipe3 = Pipe.fromConfig(
+  { id: "toUpperCase", description: "To uppercase" },
+  (input: string) => input.toUpperCase(),
+);
+
+chainPipes([pipe1, pipe2, pipe3], 1, {
+  onProgress: (completed, total) =>
+    console.log(`Progress: ${completed}/${total}`),
+})
+  .then((results) => {
+    console.log("All pipes executed successfully", results);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
