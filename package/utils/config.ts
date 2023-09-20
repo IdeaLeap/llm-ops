3e4e483cdad41df9f4f301ac742b99a953c04080
少轻狂 <1677568218@qq.com>
2023-09-20 19:45:42 +0800

    chore: 更新openai-node版本，修复header问题

--- package/utils/config.ts
+++ package/utils/config.ts
@@ -1,13 +1,11 @@
 import "dotenv/config";
 export class GWT_CONFIG {
   static OPEN_PATH = {
-    basePath: "https://oai.hconeai.com/v1",
-    baseOptions: {
-      headers: {
+    baseURL:"https://oai.hconeai.com/v1",
+    defaultHeaders: {
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
         "Helicone-Auth": `Bearer ${process.env["HELICONE_AUTH_API_KEY"]}`,
       },
-    },
   };
   static OPENAI_API_KEY = process.env["OPENAI_API_KEY"];
   static HELICONE_AUTH_API_KEY = process.env["HELICONE_AUTH_API_KEY"];
