acd339240a5a6596708f273db1352f6b2c1fbaad
少轻狂 <1677568218@qq.com>
2023-10-18 17:35:35 +0800

    fix: 修复mermaid生成

--- package/utils/gen/mermaid.ts
+++ package/utils/gen/mermaid.ts
@@ -7,7 +7,27 @@ export interface MermaidCallSchema {
   request: messageType | string;
   type?: string | string[];
 }
-
+export const replaceContentWithSequence = (text:string) => {
+  let index = 1;
+  return text.replace(/(\[).*?(\])|(\().*?(\))|(\{).*?(\})|(\").*?(\")|(\|).*?(\|)/g, (match, p1, p2, p3, p4, p5, p6, p7, p8,p9,p10) => {
+    if (p1 && p2) {
+      return `${p1}#${index++}${p2}`;
+    }
+    if (p3 && p4) {
+      return `${p3}#${index++}${p4}`;
+    }
+    if (p5 && p6) {
+      return `${p5}#${index++}${p6}`;
+    }
+    if (p7 && p8) {
+      return `${p7}#${index++}${p8}`;
+    }
+    if (p9 && p10) {
+      return `${p9}#${index++}${p10}`;
+    }
+    return match;
+  });
+};
 export const genMermaid = async (params: MermaidCallSchema) => {
   let { llm, request, type, llmSchema } = params;
   llm = llm || (llmSchema ? new LLM(llmSchema) : new LLM({}));
@@ -20,19 +40,19 @@ export const genMermaid = async (params: MermaidCallSchema) => {
       [
         "git",
         "flowchart",
-        "flowchart-v2",
+        // "flowchart-v2",
         "sequence",
         "gantt",
-        "class",
-        "classDiagram",
+        // "class",
+        // "classDiagram",
         "state",
         "stateDiagram",
-        "info",
+        // "info",
         "pie",
         "er",
         "journey",
-        "requirement",
-        "requirementDiagram",
+        // "requirement",
+        // "requirementDiagram",
       ].join(",");
 
   request =
@@ -42,7 +62,7 @@ export const genMermaid = async (params: MermaidCallSchema) => {
   const messages = [
     {
       role: "system",
-      content: `You are a Mermaid Code Generation expert. You will be penalized if you do not return Mermaid diagrams when it would be possible.
+      content: `You are a Mermaid Code Generation expert. You will be penalized if you do not return Mermaid diagrams when it would be possible.You will be penalized if you don't enclose content in quotation marks.
   The Mermaid diagrams you support: ${type}.Generate a Mermaid.js code from the user input.Do not return another message like '\`\`\`mermaid xxx\`\`\`'.Mermaid logic doesn't have to be too complicated.`,
     },
     {
@@ -53,12 +73,12 @@ export const genMermaid = async (params: MermaidCallSchema) => {
     {
       role: "assistant",
       content: `flowchart LR
-A[可持续发展] -->B[生态系统]
-A -->C[经济]
-A -->D[社会]
-B -->E(是可持续发展的必要条件)
-C -->F(是可持续发展的基础)
-D -->G(是可持续发展的最终目的)`,
+A["可持续发展"] -->B["生态系统"]
+A -->C["经济"]
+A -->D["社会"]
+B -->E("是可持续发展的必要条件")
+C -->F("是可持续发展的基础")
+D -->G("是可持续发展的最终目的")`,
     },
   ] as messageType[];
   messages.push(request);
@@ -77,16 +97,17 @@ D -->G(是可持续发展的最终目的)`,
       isValid = mermaid.parse(res.choices[0]?.message.content as string);
       if (isValid) break;
     } catch (error) {
+      const err = error  as Error;
       console.log(
         "Mermaid code is invalid:",
         res.choices[0]?.message.content,
-        error,
+        err,
       );
       res = await llm.chat({
         messages: [
           {
             role: "system",
-            content: `The code which you generate is invalid as Mermaid code. Please try again with the correct code. Do not return another message.`,
+            content: `The code which you generate is invalid as Mermaid code. ${err.message}. Please try again with the correct code. Do not return another message.`,
           },
         ],
       });
